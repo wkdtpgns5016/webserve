a1c4ef18ed0d6002395b1d1d7d1fb332eb68e5b4
sunwchoi <chltjsdn103@naver.com>
2023-06-06 17:05:22 +0900

    Feat: WebServer 객체 리팩토링
    1. WebServer객체 리펙토링
    
    Related to #113

--- webserve/WebServer/WebServer.cpp
+++ webserve/WebServer/WebServer.cpp
@@ -1,4 +1,5 @@
 #include "WebServer.hpp"
+#include <map>
 
 WebServer::WebServer()
 {
@@ -6,43 +7,61 @@ WebServer::WebServer()
 
 WebServer::~WebServer()
 {
-    std::vector<Server *>::iterator iter = _servers.begin();
-    for (; iter != _servers.end(); iter++)
-    {
-        if (*iter != NULL)
-        {
-            delete (*iter);
-            *iter = NULL;
-        }
-    }
+    std::map<int, Server *>::iterator ite = _servers.end();
+    for (std::map<int, Server *>::iterator it; it != ite ; it++)
+		delete (*it).second;
 }
+
+int	WebServer::initSocket(int port, unsigned int ip)
+{
+	int	fd;
+	struct	sockaddr_in	addr;
+	int	optvalue = 1;
+
+	if ((fd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+		exit(1);
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_addr.s_addr = ip;
+	addr.sin_port = htons(port);
+
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optvalue, sizeof(optvalue));
+	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+		exit(1);
+	if (listen(fd, 1000) == -1)
+		exit(1);
+	return fd;
+}
+
 
 WebServer::WebServer(const Conf &conf)
 {
-    std::vector<Block*> s_blocks = conf.getInnerBlock();
-    std::vector<Block*>::iterator iter = s_blocks.begin();
-    for (;iter != s_blocks.end(); iter++)
-    {
-        ServerBlock* server_block = dynamic_cast<ServerBlock *>(*iter);
-        Server* server = new Server(server_block);
-        _servers.push_back(server);
-    }
+	ServerBlock* block;
+	std::vector<ServerBlock*>	blocks;
+	std::pair<int, unsigned int> port_addr;
+	int	fd;
+	std::map<std::pair<int, unsigned int>, std::vector<ServerBlock*>> server_blocks;
+
+	std::vector<Block*> inner_blocks = conf.getInnerBlock();
+	std::vector<Block*>::iterator ite= inner_blocks.end();
+	for (std::vector<Block*>::iterator it= inner_blocks.begin(); it != ite; it++)
+	{
+		port_addr = std::make_pair((*it)->getPort(), (*it)->getAddr());
+		block = dynamic_cast<ServerBlock*>(*it);
+		server_blocks[port_addr].push_back(block);
+	}
+
+	std::map<std::pair<int, unsigned int>, std::vector<ServerBlock*>>::iterator	ite1 = server_blocks.end();
+	for(std::map<std::pair<int, unsigned int>, std::vector<ServerBlock*>>::iterator it = server_blocks.begin(); it != ite1; it++)
+	{
+		port_addr = (*it).first;
+		blocks = (*it).second;
+		fd = initSocket(port_addr.first, port_addr.second);
+		_servers[fd] = new Server(fd, blocks);
+	}
 }
 
 void WebServer::run(void)
 {
-    int last_port = -1;
-    std::vector<Server *>::iterator iter = _servers.begin();
-    for (; iter != _servers.end(); iter++)
-    {
-        if ((*iter)->getPort() != last_port)
-            (*iter)->threading();
-        last_port = (*iter)->getPort();
-    }
-    iter = _servers.begin();
-    for (; iter != _servers.end(); iter++)
-    {
-        pthread_t tmp = (*iter)->getThread();
-        pthread_join(tmp, NULL);
-    }
 }
